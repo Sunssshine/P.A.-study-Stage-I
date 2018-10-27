2fdca9dfcdf985ed7ba1d9e74bc162c85ffc1134
Sunssshine <deadlyshine@gmail.com>
2018-10-27 21:53:26 +0300

    Hello world (initial commit)

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+int hello (int test, int kek)
+{
+    return test+kek;
+}
+
+int main()
+{
+    
+    printf("Hello World!\n");
+    printf("\n-%d\n", hello(10,20));
+    return 0;
+}
