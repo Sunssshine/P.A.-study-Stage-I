9fccc229129e37576c17ff74b4240296962e9e3b
Sunssshine <deadlyshine@gmail.com>
2018-10-28 01:28:00 +0300

    Create

--- test.c
+++ test.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-int hello (int test, int kek)
-{
-    return test+kek;
-}
 
 int main()
 {
-    
-    printf("Hello World!\n");
-    printf("\n-%d\n", hello(10,20));
+    pid_t p;
+    p = fork();
+    if(p)
+    {
+        printf("In parent:\n - Current process pid = %d\n - Child process pid = %d\n - Parent process pid = %d\n\n", getpid(), p, getppid());
+    }
+    else
+    {
+        printf("In child:\n - Current process pid = %d\n - Parent process pid = %d\n", getpid(), getppid());
+    }
+
+
+    //printf("Hello World!\n");
     return 0;
+    
+    
 }
